40c852858a0124fe774aed0d3d67d90bd9afafd7
Samuel Annin Yeboah <35128520+saytoonz@users.noreply.github.com>
2023-07-20 16:32:30 +0000

    Converted the search loop to iterator adaptor
    Note: Performance is not affected just
    choice and other way of doing same thing as well as simplicity

--- src/lib.rs
+++ src/lib.rs
@@ -50,14 +50,17 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
+    // let mut result = Vec::new();
+    // for line in contents.lines() {
+    //     if line.contains(query){
+    //         result.push(line);
+    //     }
+    // }
+    // result
 
-    for line in contents.lines() {
-        if line.contains(query){
-            result.push(line);
-        }
-    }
-    result
+    contents.lines()
+    .filter(|line| line.contains(query))
+    .collect()
 }
 
 
